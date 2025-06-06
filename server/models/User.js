69ab968d17089d3920adb57f8d43951c36d9333d
Amr Sabsabi <amrsabsabe.2003.as@gmail.com>
2025-06-07 02:49:56 +0300

    edited user

--- server/models/User.js
+++ server/models/User.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
     {
-        _id :{ type: String , requierd: true},
-        name :{ type: String , requierd: true},
-        email:{ type: String , requierd: true},
-        imageUrl: {type: String , requierd: true},
+        _id :{ type: String , required: true},
+        name :{ type: String , required: true},
+        email:{ type: String , required: true},
+        imageUrl: {type: String , required: true},
         enrolledCourses:[
             {
                 type: mongoose.Schema.Types.ObjectId,
